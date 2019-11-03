b4d0d48a905d436f13bfaf2bb88588d2064127e6
Shanna Kershner <shanna.kershner@gmail.com>
2019-11-03 05:11:59 +0000

    final commit!

--- linkedlist1.rb
+++ linkedlist1.rb
@@ -1,12 +1,20 @@
-class LinkedList
+class LinkedListNode
+  attr_accessor :value, :next_node
 
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
 
-  print_values(node3)
-  puts "---------"
-  revlist = reverse_list(node3)
-  print_values(revlist)
-
-
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} -->"
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
 end
 
 class Stack
@@ -17,33 +25,32 @@ class Stack
   end
 
   def push(value)
-
+    @data = LinkedListNode.new(value, @data)
   end
 
   def pop
+    return puts "nil\n" if @data.nil?
+    puts "#{@data.value}\n"
+    @data = @data.next_node
+  end
+end
 
+def reverse_list(list)
+  stack = Stack.new
+  while list
+    stack.push(list.value)
+    list = list.next_node
   end
 
-  def reverse_list(list)
-    while list
-      #call list.value to access current value of LinkedListNode
-      list = list.next_node
-    end
-
-  def print_values(list_node)
-    if list_node
-      print"#{list_node.value} --> "
-      print_values(list_node.next_node)
-    else
-      print "nil\n"
-      return
-    end
-  end
-
+ return stack.data
 end
 
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
 
+
 print_values(node3)
+puts "---------"
+revlist = reverse_list(node3)
+print_values(revlist)
